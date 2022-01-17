1ace51a69dc57409642311a5279573eaec5f73ae
dimassdoubles <codedimas@gmail.com>
2022-01-17 20:50:06 +0700

    berhasil content-dashboard

---
+++ web-admin/content_dashboard.php
@@ -0,0 +1,24 @@
+<table style="width: 100%; text-align:center; margin-top: 24px">
+    <tr>
+        <td>
+            <h1 class="h1" style="color: #1A2732">1</h1>
+        </td>
+        <td>
+            <h1 class="h1" style="color: #1A2732">0</h1>
+        </td>
+        <td>
+            <h1 class="h1" style="color: #1A2732">12</h1>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <h3 class="h1" style="color: #1A2732">Jumlah User</h3>
+        </td>
+        <td>
+            <h3 class="h1" style="color: #1A2732">Jumlah Pesanan</h3>
+        </td>
+        <td>
+            <h3 class="h1" style="color: #1A2732">Jumlah Komentar</h3>
+        </td>
+    </tr>
+</table>
